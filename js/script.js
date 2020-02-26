9b8e2f18a4118d5edee4da33ae36b096aa1a1484
airizarry2 <54568019+airizarry2@users.noreply.github.com>
2020-02-26 17:33:44 -0600

    fixed method call issue and commented

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 function validateForm() {
+	//Form validation for first name field
   var first = document.forms["myForm"]["firstname"].value;
   if (first == "") {
     alert(" First Name must be filled out");
     return false;
   }
- 
+ //Form Validation for lastname field
   var last = document.forms["myForm"]["lastname"].value;
   if (last == "") {
     alert("Last name must be filled out");
     return false;
   }
+  
+    //Form validation for Email field
   var email = document.forms["myForm"]["email"].value;
   if (email == "") {
     alert("Email must be filled out");
     return false;
   }
+  //Form validation for Phone number field
    var phone = document.forms["myForm"]["phone"].value;
   if (phone == "") {
     alert("Phone Number must be filled out");
@@ -22,4 +26,5 @@ function validateForm() {
   }
 }
 
-validateForm();
+
+
