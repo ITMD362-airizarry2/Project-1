6a27d4d804c4807042aeb8f212973cb56abb6148
airizarry2 <54568019+airizarry2@users.noreply.github.com>
2020-02-26 17:17:26 -0600

    added form validation for input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+function validateForm() {
+  var x = document.forms["myForm"]["firstname"].value;
+  if (x == "") {
+    alert(" First Name must be filled out");
+    return false;
+  }
+ 
+  var x = document.forms["myForm"]["lastname"].value;
+  if (x == "") {
+    alert("Last name must be filled out");
+    return false;
+  }
+  var x = document.forms["myForm"]["email"].value;
+  if (x == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+   var x = document.forms["myForm"]["phone"].value;
+  if (x == "") {
+    alert("Phone Number must be filled out");
+    return false;
+  }
+}
 
+validateForm();
